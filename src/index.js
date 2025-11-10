ffb31558c4205b1c5d2c557d1e04ec4b4d93d2db
Leon Chapman <lchapman9955@gmail.com>
2025-11-10 19:29:55 +0000

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.js 	new file:   src/StarRating.js 	new file:   src/index.css 	new file:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import StarRating from "./StarRating.js";
+
+function Test() {
+  const [movieRating, setMovieRaing] = React.useState(0);
+
+  return (
+    <div>
+      <StarRating colour="blue" maxRating={10} onSetRating={setMovieRaing} />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <Test />
+  </React.StrictMode>
+);
